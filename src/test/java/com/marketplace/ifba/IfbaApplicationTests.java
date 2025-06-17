db7c3793fd23fcd40da043e948cf7fc73c39bfc1
LuisMiguelADS <luisbahia081@gmail.com>
2025-06-17 18:41:04 -0300

    Initial Commit
    Criação do ambiente de desenvolvimento.

---
+++ src/test/java/com/marketplace/ifba/IfbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.ifba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
